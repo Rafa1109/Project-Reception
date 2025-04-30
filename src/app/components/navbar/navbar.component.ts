05066c1c6ec56da97f67fee89b9ffdca8fa4d919
Nilo Jose de Andrade Neto <nneto.kstack@veloe.com.br>
2025-04-29 23:03:02 -0300

    fix

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { MenuItem } from "primeng/api";
 import { AuthService } from "src/app/core/services/authentications/auth.service";
+import { PermissionService } from "src/app/core/services/authentications/permission.service";
 
 
 @Component({
@@ -11,6 +12,7 @@ import { AuthService } from "src/app/core/services/authentications/auth.service"
 export class NavBarComponent implements OnInit {
 
     constructor(private authService: AuthService,
+        private permissionService: PermissionService,
         private route: Router,
     ) { }
 
@@ -20,26 +22,34 @@ export class NavBarComponent implements OnInit {
     ngOnInit(): void {
         this.tokenExpirou = this.authService.tokenExpire();
 
-        this.items = [
-            {
-                label: this.tokenExpirou ? 'Login' : 'Logout',
-                icon: 'pi pi-user',
-                command: () => {
-                    if (this.tokenExpirou) {
-                        this.route.navigate(['/login']);
-                    } else {
-                        this.authService.logout();
-                    }
+        // Adiciona login/logout sempre
+        this.items.push({
+            label: this.tokenExpirou ? 'Login' : 'Logout',
+            icon: 'pi pi-user',
+            command: () => {
+                if (this.tokenExpirou) {
+                    this.route.navigate(['/login']);
+                } else {
+                    this.authService.logout();
                 }
-            },
-            {
+            }
+        });
+
+        // Exemplo: só mostra "Avisos" se tiver role "ROLE_USER_READ"
+        if (this.permissionService.hasPermissionButton("ROLE_USER_READ")) {
+            this.items.push({
                 label: 'Avisos',
                 command: () => this.route.navigate(['/avisos'])
-            },
-            {
+            });
+        }
+
+        // Exemplo: só mostra "Histórico" se tiver role "ROLE_USER_WRITER"
+        if (this.permissionService.hasPermissionButton("ROLE_USER_WRITER")) {
+            this.items.push({
                 label: 'Histórico',
                 command: () => this.route.navigate(['/historico'])
-            }
-        ];
+            });
+        }
+
     }
 }
